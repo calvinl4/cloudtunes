25ee631915926ac1ad463efc165ff26e437fee1e
Calvin Lin <calvinlin@live.ca>
2019-09-15 01:26:46 -0400

    finished graphics and http request

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,32 @@
 console.log('Client side javascript is loaded.')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response)=> {
+//     response.json().then((data)=> {
+//         console.log
+//     })
+// })
+ 
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    fetch('http://localhost:3000/weather?address=' + location).then((response)=> {
+        response.json().then((data)=> {
+            if(data.error){
+                console.log('Big Error')
+            } else {
+                messageOne.textContent = ('Location: ' + data.location)
+                messageTwo.textContent = (data.forecast)
+            }
+        })
+    })
+})
